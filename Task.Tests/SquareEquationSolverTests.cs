c84f6c9bd97db3af236c8d2f5f8a8015d208dd13
Anastasia Kravtsova <anastasya.kallisto@gmail.com>
2023-09-12 17:14:46 +0600

    fix wrong answer in test

--- Task.Tests/SquareEquationSolverTests.cs
+++ Task.Tests/SquareEquationSolverTests.cs
@@ -5,7 +5,7 @@ namespace Task.Tests
     {
 
         [Theory]
-        [InlineData(2, 5, -3.5, -3.0, 0.5)]
+        [InlineData(2, 5, -3.5, -3.070027, 0.570027)]
         [InlineData(1, 2, 1, -1.0)]
         [InlineData(1, 0, 1)]
         [InlineData(1, 4, 1, -3.732051, -0.267949)]
@@ -22,7 +22,7 @@ namespace Task.Tests
             Assert.Equal(expectedRoots.Length, result.Length);
             for (int i = 0; i < expectedRoots.Length; i++)
             {
-                Assert.Equal(expectedRoots[i], result[i], 1E-1);
+                Assert.Equal(expectedRoots[i], result[i], 1E-6);
             }
         }
     }
